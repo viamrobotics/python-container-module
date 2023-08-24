76880680770dbd60de5c2ff1c7857d2583ad6ef3
Abe Winter <abe-winter@users.noreply.github.com>
2023-08-23 21:30:13 -0400

    add logging to detect get_readings issues

--- module/__main__.py
+++ module/__main__.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, logging
 from typing import Any, Dict, Mapping, Optional
 
 from viam.components.sensor import Sensor
@@ -9,6 +9,8 @@ from viam.resource.types import Model, ModelFamily
 from viam.module.module import Module
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 
+logger = logging.getLogger(__name__)
+
 class MySensor(Sensor):
     MODEL = Model(ModelFamily("viam","wifi_sensor"), "linux")
 
@@ -18,8 +20,10 @@ class MySensor(Sensor):
         return sensor
 
     async def get_readings(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> Mapping[str, Any]:
+        logger.info("get_readings top")
         with open("/proc/net/wireless") as wifi_stats:
             content = wifi_stats.readlines()
+        logger.info("get_readings ok read")
         wifi_signal = [x for x in content[2].split(" ") if x != ""]
         return {"link": wifi_signal[2], "level": wifi_signal[3], "noise": wifi_signal[4]}
 
@@ -29,6 +33,7 @@ class MySensor(Sensor):
 # Anything below this line is optional and will be replaced later, but may come in handy for debugging and testing.
 # To use, call `python wifi_sensor.py` in the command line while in the `src` directory.
 async def main():
+    logging.basicConfig(level=logging.INFO)
     Registry.register_resource_creator(MySensor.SUBTYPE, MySensor.MODEL, ResourceCreatorRegistration(MySensor.new))
     module = Module.from_args()
     module.add_model_from_registry(MySensor.SUBTYPE, MySensor.MODEL)
